8b4a75a13f0edf7331109cfd046d72466c266d43
jzx531 <115254510+jzx531@users.noreply.github.com>
2026-02-28 12:03:26 +0800

    Day8 微信订单提交

---
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -0,0 +1,84 @@
+package com.sky.mapper;
+
+import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
+import com.sky.dto.OrdersPageQueryDTO;
+import com.sky.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface OrderMapper {
+    /**
+     * 插入订单数据
+     * @param orders
+     */
+    void insert(Orders orders);
+
+    /**
+     * 根据订单号和用户id查询订单
+     * @param orderNumber
+     * @param userId
+     * @return
+     */
+    Orders getByNumberAndUserId(@Param("orderNum")String orderNumber, @Param("userId")Long userId);
+
+    /**
+     * 修改订单信息
+     * @param orders
+     */
+    void update(Orders orders);
+
+    /**
+     * 分页条件查询
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 根据id查询订单
+     * @param id
+     * @return
+     */
+    Orders getById(Long id);
+
+    /**
+     * 根据状态查询订单数量
+     * @param status
+     * @return
+     */
+    Integer countStatus(Integer status);
+
+    /**
+     * 根据状态和下单时间查询订单
+     * @param status
+     * @param OrderTime
+     */
+    List<Orders> getByStatusAndOrderTime(@Param("status")Integer status, @Param("startTime")LocalDateTime OrderTime);
+
+    /**
+     * 根据动态条件统计营业额
+     * @param map
+     * @return
+     */
+    Double sumByMap(Map map);
+
+    /**
+     * 根据动态条件统计订单数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
+
+    /**
+     * 查询商品销量排名
+     * @param begin
+     * @param end
+     */
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
+}
